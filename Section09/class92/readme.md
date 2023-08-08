be48fcba19f0dcafdeaef60acfa1c529254e8922
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2023-08-08 08:45:00 -0300

    Restart Section09

--- Section09/class92/readme.md
+++
@@ -1,8 +0,0 @@
-# Babel and "Can I use..."
-
-
-**Commands List:**
-
-- npm init -y
-- npm install --save-dev @babel/cli @babel/preset-env @babel/core
-- npx babel main.js -o bundle.js --presets=@babel/env
--- Section09/class94/src/index.js
+++
@@ -1,9 +0,0 @@
-// import * as Module1 from './module1';
-
-import Person, { firstName, lastName, age, sum } from './module1';
-
-const person = new Person (firstName, lastName, age)
-
-console.log(person);
-
-console.log('Sum = ' + sum(5, 8));
