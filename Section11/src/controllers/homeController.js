eb832dc5558a416fdb9f6285b6171b916eb7e0c9
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-20 19:11:45 +0000

    update - home + 404

--- Section11/src/controllers/homeController.js
+++ Section11/src/controllers/homeController.js
@@ -1,12 +1,4 @@
-exports.homepage = (req, res) => {
-    res.render('index', {
-        title: 'This is my new Homepage Title'
-    });
-    return;
-}
-
-exports.sendForm = (req, res) => {
-    res.send(`New POST route message: ${req.body.inputName}`);
-
+exports.index = (req, res) => {
+    res.render('index');
     return;
 }
