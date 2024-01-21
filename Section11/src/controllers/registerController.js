25ef65c9c5e12d359480e218b595be440b1e2723
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-21 11:53:56 +0000

    update - register controller

--- Section11/src/controllers/registerController.js
+++ Section11/src/controllers/registerController.js
@@ -1,10 +1,34 @@
+const Register = require('../models/RegisterModel');
+const { login } = require('./loginController');
+
 exports.index = (req, res) => {
     res.render('register');
-    return;
 }
 
-exports.register = (req, res) => {
-    res.send('<h1>Register Route Success</h1>');
-    console.log(req.body);
-    return;
+exports.register = async function(req, res) {
+    try {
+        const register = new Register(req.body);
+
+        await register.register();
+    
+        if(register.errors.length > 0) {
+            req.flash('errors', register.errors);
+    
+            req.session.save(function() {
+                return res.redirect('/register');
+            });
+            return;
+        }
+    
+        req.flash('success', 'Registration Successful. Login to access your Agenda!');
+    
+        req.session.save(function() {
+            return res.redirect('/register');
+        });
+        return;
+        
+    } catch(e) {
+        console.log(e);
+        res.render('404');
+    }
 }
