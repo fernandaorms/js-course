227e71eed8eaa18fd28f32c32bc5dc93a5940f7f
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-21 21:00:01 +0000

    update imports - use webpack to handle with bootstrap

--- Section11/frontend/main.js
+++ Section11/frontend/main.js
@@ -1,4 +1,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './assets/css/style.css';
