2c6c767aa7fed1342929f5d6c4bca94a3f85c228
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-21 11:52:06 +0000

    update loginController

--- Section11/src/controllers/loginController.js
+++ Section11/src/controllers/loginController.js
@@ -1,10 +1,7 @@
 exports.index = (req, res) => {
     res.render('login');
-    return;
 }
 
 exports.login = (req, res) => {
-    res.send('<h1>Login Route Success</h1>');
-    console.log(req.body);
-    return;
+    res.send(req.body);
 }
