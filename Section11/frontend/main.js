dfbc22426908688973483b19c0bb92b983d2e7f4
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2024-01-23 23:17:42 +0000

    update - init form validation (frontend)

--- Section11/frontend/main.js
+++ Section11/frontend/main.js
@@ -6,3 +6,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './assets/css/style.css';
+
+
+import Form from './modules/Form';
+import FormLogin from './modules/FormLogin';
+import FormContact from './modules/FormContact';
+import FormAccount from './modules/FormAccount';
+
+const register = new Form('register');
+const login = new FormLogin('login');
+const contact = new FormContact('contact');
+const account = new FormAccount('account');
+
+register.init();
+login.init();
+contact.init();
+account.init();
